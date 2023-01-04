5f6663891977a61031bc6e572ec0ff71a49b6d90
Ihar Andreyeu <>
2023-01-04 21:33:53 +0400

    Rework task test

--- Tests/SubscriptionsTests/TaskTests.swift
+++ Tests/SubscriptionsTests/TaskTests.swift
@@ -2,6 +2,20 @@ import Subscriptions
 import XCTest
 
 final class TaskTests: TestCase {
+    func test_asyncTaskIsCancelledOnCancelAll() {
+        let store = SubscriptionsStore()
+        
+        let subscription = Task.sleep(seconds: 300)
+        subscription.store(in: store)
+        
+        store.cancelAll()
+        
+        XCTAssert(
+            subscription.isCancelled,
+            "Expected task to be cancelled, but it did not"
+        )
+    }
+    
     func test_taskIsCancelledUponStoreDeinit() {
         var store: SubscriptionsStore!
         let subscription = Task.sleep(seconds: 300)
